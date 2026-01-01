5c4d3fb1c5f7a219b3c88e79690fce0171445021
arjakroy <arjakroy2411@gmail.com>
2026-01-01 13:54:15 +0530

    commit2

--- config/db.js
+++ config/db.js
@@ -1,21 +1,23 @@
 const { Pool } = require('pg');
 
 const client = new Pool({
-    user: process.env.ADMIN_USER || '',
-    host: process.env.DB_HOST || '',
-    database: process.env.DB_NAME || '',
-    password: process.env.ADMIN_PASS || '',
-    port: 5432,
-    statement_timeout: 1000, // 1 seconds
+    // user: process.env.ADMIN_USER || '',
+    // host: process.env.DB_HOST || '',
+    // database: process.env.DB_NAME || '',
+    // password: process.env.ADMIN_PASS || '',
+    // port: 5432,
+    // statement_timeout: 1000, // 1 seconds
+    connectionString: process.env.ADMINCONNECTIOION
 });
 
 const studentpool = new Pool({
-    user: process.env.STUDENT_USER || '',
-    host: process.env.DB_HOST || '',
-    database: process.env.DB_NAME || '',
-    password: process.env.STUDENT_PASS || '',
-    port: 5432,
-    statement_timeout: 2000, // 1 seconds
+    // user: process.env.STUDENT_USER || '',
+    // host: process.env.DB_HOST || '',
+    // database: process.env.DB_NAME || '',
+    // password: process.env.STUDENT_PASS || '',
+    // port: 5432,
+    // statement_timeout: 2000, // 1 seconds
+    connectionString: process.env.STUDENTCONNECTION
 });
 
 module.exports = { client, studentpool };
